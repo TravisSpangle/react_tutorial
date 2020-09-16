7dd184352ec10e232c4f0a2b35e25b8e286e7a44
Travis Spangle <Travis.Spangle@GettyImages.com>
2020-09-16 15:30:17 -0700

    Showing the Past Moves

--- src/index.js
+++ src/index.js
@@ -11,13 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state ={
-  //     squares: Array(9).fill(null),
-  //     xIsNext: true,
-  //   };
-  // }
 
   renderSquare(i) {
     return (
@@ -83,9 +76,20 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[history.length -1];
-    const winner = calculateWinner(current.squares);
-    let status;
+    const winner  = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+	'Go to move #' + move :
+	'Go to game start';
+      return (
+	<li>
+	  <button onClick={() => this.jumpTo(move)}>{desc}</button>
+	</li>
+      );
+    });
+
+    let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
@@ -102,12 +106,16 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
 	  <div>{status}</div>
-	  <ol>{/* TODO */}</ol>
+	  <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 function calculateWinner(squares) {
   const lines = [
@@ -128,7 +136,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById('root'));
