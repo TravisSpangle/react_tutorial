8cac681a332b36efa524b22e14b2002cf910d8d0
Travis Spangle <Travis.Spangle@GettyImages.com>
2020-09-16 12:30:24 -0700

    taking turns

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state ={
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -36,21 +40,21 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+	<div className="board-row">
+	  {this.renderSquare(0)}
+	  {this.renderSquare(1)}
+	  {this.renderSquare(2)}
+	</div>
+	<div className="board-row">
+	  {this.renderSquare(3)}
+	  {this.renderSquare(4)}
+	  {this.renderSquare(5)}
+	</div>
+	<div className="board-row">
+	  {this.renderSquare(6)}
+	  {this.renderSquare(7)}
+	  {this.renderSquare(8)}
+	</div>
       </div>
     );
   }
